f0c1e8b62bed43012757b4c80a54908b5eca2f3d
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-01 14:12:02 -0500

    se agrega mas consultas a la db

--- spring-jpa/src/main/java/com/spring/jpa/springjpa/repository/ICustomerRepository.java
+++ spring-jpa/src/main/java/com/spring/jpa/springjpa/repository/ICustomerRepository.java
@@ -2,10 +2,20 @@ package com.spring.jpa.springjpa.repository;
 
 import com.spring.jpa.springjpa.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ICustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByFirstName(String name);
+
+    List<Customer> findByFirstNameContaining(String firstName);
+
+    List<Customer> findByLastNameNotNull();
+    List<Customer> findByAddress_City(String city);
+
+    @Query("select c from Customer c where c.email = ?1")
+    Customer getCustomerByEmailAddress(String email);
 }
