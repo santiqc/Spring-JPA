63f8cfc934fc992f19fed46e63cbd71e7ccb83cc
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-01 15:45:53 -0500

    creacion de la entidad Local y Manager
    Tambien por parte de la entidad local se hace una relacion de uno a uno con la entidad Manager

---
+++ spring-jpa/src/main/java/com/spring/jpa/springjpa/entity/Local.java
@@ -0,0 +1,37 @@
+package com.spring.jpa.springjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//@ToString(exclude = "manager")
+public class Local {
+
+    @Id
+    @SequenceGenerator(
+            name = "local_secuence",
+            sequenceName = "local_secuence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "local_secuence")
+    private Long localId;
+    private String name;
+    private String floor;
+
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            name = "manager_id",
+            referencedColumnName = "managerId"
+    )
+    private Manager manager;
+
+}
---
+++ spring-jpa/src/main/java/com/spring/jpa/springjpa/entity/Manager.java
@@ -0,0 +1,30 @@
+package com.spring.jpa.springjpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Manager {
+
+    @Id
+    @SequenceGenerator(
+            name = "manager_sequence",
+            sequenceName = "manager_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "manager_sequence"
+    )
+    private Long managerId;
+    private String firstName;
+    private String lastName;
+
+}
