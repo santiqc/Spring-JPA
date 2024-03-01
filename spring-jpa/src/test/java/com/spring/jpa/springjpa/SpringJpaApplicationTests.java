036e3506dc500385a08c9faa9096bc124ec105e4
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-01 11:08:09 -0500

    Creacion del proyecto y conexion a la BD

---
+++ spring-jpa/src/test/java/com/spring/jpa/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.jpa.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
